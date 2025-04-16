e249095a45c19ce99d7ff183962d1b5b791ca9cc
Miguel Caballer <micafer1@upv.es>
2025-04-16 10:27:43 +0200

    Add chameleon cloud provider

--- app/__init__.py
+++ app/__init__.py
@@ -1012,7 +1012,7 @@ def create_app(oidc_blueprint=None):
         image = None
         priv_network_id = None
         pub_network_id = None
-        if cred_data['type'] in ['fedcloud', 'OpenStack', 'OpenNebula', 'Linode', 'Orange', 'GCE', 'CH', 'EUNode']:
+        if cred_data['type'] in ['fedcloud', 'OpenStack', 'OpenNebula', 'Linode', 'Orange', 'GCE', 'CH', 'EUNode', 'Chameleon']:
             if cred_data['type'] == 'fedcloud':
                 site, _, vo = utils.get_site_info(cred_id, cred, get_cred_id())
                 if "networks" in site and vo in site["networks"]:
