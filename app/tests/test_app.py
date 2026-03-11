e9b79d0476785a36d3f6f8f173e9bb756d5619bd
Miguel Caballer <micafer1@upv.es>
2026-03-11 17:05:36 +0100

    Fix test

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -106,7 +106,7 @@ class IMDashboardTests(unittest.TestCase):
             resp.ok = True
             resp.status_code = 200
             resp.json.return_value = {"outputs": {"key": "value", "key2": "http://server.com",
-                                                  "key3": "https://[server2.com,server3.com]/path"}}
+                                                  "key3": "https://['server2.com','server3.com']/path"}}
         elif url == "/im/infrastructures/infid/radl":
             resp.ok = True
             resp.status_code = 200
@@ -457,8 +457,8 @@ class IMDashboardTests(unittest.TestCase):
         self.assertIn(b'key2', res.data)
         self.assertIn(b'value', res.data)
         self.assertIn(b"<a href='http://server.com' target='_blank'>http://server.com</a>", res.data)
-        self.assertIn(b"<a href='http://server2.com' target='_blank'>http://server2.com</a>", res.data)
-        self.assertIn(b"<a href='http://server3.com' target='_blank'>http://server3.com</a>", res.data)
+        self.assertIn(b"<a href='https://server2.com/path' target='_blank'>https://server2.com/path</a>", res.data)
+        self.assertIn(b"<a href='https://server3.com/path' target='_blank'>https://server3.com/path</a>", res.data)
 
     @patch("app.utils.getUserAuthData")
     @patch('requests.delete')
