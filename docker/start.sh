5d021597b966f0f39b15adeb6f5e858f698aad79
Miguel Caballer <micafer1@upv.es>
2026-05-13 10:28:53 +0200

    Move to granian

--- docker/start.sh
+++ docker/start.sh
@@ -1,14 +1,16 @@
-#!/bin/bash
+#!/bin/sh
+set -eu
 
+APP_TARGET="imdashboard:app"
+BASE_ARGS="--interface wsgi --host 0.0.0.0 --port 5001 --workers ${WORKERS:-1} --blocking-threads ${THREADS:-1}"
 
-
-if [ "${ENABLE_HTTPS}" == "True" ]; then
-  if test -e /certs/cert.pem && test -f /certs/key.pem ; then
-    exec gunicorn --bind 0.0.0.0:5001 -k "$WORKER_TYPE" -w "$WORKERS" --certfile /certs/cert.pem --keyfile /certs/key.pem --timeout "$TIMEOUT"  imdashboard:app
+if [ "${ENABLE_HTTPS:-False}" = "True" ]; then
+  if test -e /certs/cert.pem && test -f /certs/key.pem; then
+    exec granian ${BASE_ARGS} --ssl-certificate /certs/cert.pem --ssl-keyfile /certs/key.pem "$APP_TARGET"
   else
     echo "[ERROR] File /certs/cert.pem or /certs/key.pem NOT FOUND!"
     exit 1
   fi
 else
-  exec gunicorn --limit-request-line 8190 --bind 0.0.0.0:5001 -k "$WORKER_TYPE" -w "$WORKERS" --timeout "$TIMEOUT" --threads "$THREADS" imdashboard:app
+  exec granian ${BASE_ARGS} "$APP_TARGET"
 fi
